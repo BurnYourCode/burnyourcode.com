588c6bf5530f0f79c86d29d0b15ab653746f75dd
Mitesh Shah <mitesh@miteshshah.com>
2020-07-03 19:15:58 +0530

    feat: updated ghpublish

--- ghpublish.sh
+++ ghpublish.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# One time setup
+# git checkout --orphan gh-pages
+# git reset --hard
+# git commit --allow-empty -m "Initializing gh-pages branch"
+# git push origin gh-pages
+# git checkout master
+
 if [ "`git status -s`" ]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -13,7 +20,7 @@ git worktree prune
 rm -rf .git/worktrees/_build/
 
 echo "Checking out gh-pages branch into _build"
-git worktree add -B gh-pages _build upstream/gh-pages
+git worktree add -B gh-pages _build origin/gh-pages
 
 echo "Removing existing files"
 rm -rf _build/*
@@ -24,5 +31,5 @@ make build
 echo "Updating gh-pages branch"
 cd _build && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-#echo "Pushing to github"
-#git push --all
+echo "Pushing to github"
+git push --all
